12b61b159ffc9fe4c1253dd5223a29e653bae630
HalaAlshokri <119620071+HalaAlshokri@users.noreply.github.com>
2023-10-27 10:54:31 +0300

    Put object detection

--- lib/model/ObjectCount.py
+++ lib/model/ObjectCount.py
@@ -1,9 +1,22 @@
+import timeit
 import schedule 
+import os
 import time 
 import numpy as np
 import cv2
 from ultralytics import YOLO
 from keras.models import load_model  # TensorFlow is required for Keras to work
+import math
+
+
+from firebase_admin import firestore
+#credentials.Certificate
+cred = r"C:\GithubRepos\Hima-Local-Repo\Hima-Front-End\lib\model\hima-front-end-firebase-adminsdk-ilbe6-21df361235.json" #C:\GithubRepos\Hima-Local-Repo\Hima-Front-End\lib\model\hima-front-end-firebase-adminsdk-ilbe6-21df361235.json
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = cred
+#firebase_admin.initialize_app(cred)
+
+
+db = firestore.Client()
 
 # Disable scientific notation for clarity // keras
 np.set_printoptions(suppress=True)
@@ -11,7 +24,7 @@ np.set_printoptions(suppress=True)
 # Load the models
 TMmodel = load_model(r"C:\GithubRepos\Hima-Local-Repo\Hima-Front-End\lib\model\keras_model.h5", compile=False)
 #YOLOmodel=YOLO('yolov8l') #to be deleted
-YOLOmodel=YOLO(r'C:\GithubRepos\Hima-Local-Repo\Hima-Front-End\lib\model\best(YOLOv8L).pt') #Loading our custom model
+YOLOmodel=YOLO(r"C:\GithubRepos\Hima-Local-Repo\Hima-Front-End\lib\model\YOLOv8L(368).pt") #Loading our custom model
 
 # Load the labels
 class_names = ["Normal", "Crowd"]
@@ -27,20 +40,21 @@ sources_array=[IMAGE_SOURCE1,IMAGE_SOURCE2,IMAGE_SOURCE3,IMAGE_SOURCE4]
 
 # creating a dictionary (Area : Officers)
 areas = {
-    "Zone A": 25, 
-    "Zone B": 25,
-    "Zone C": 25,
-    "Zone D": 25
+    "Zone-A": 25, 
+    "Zone-B": 25,
+    "Zone-C": 25,
+    "Zone-D": 25
 }
 
 #Method to predict number of heads in a single frame
 def predict(IMAGE_SOURCE):
     sucess, frame = IMAGE_SOURCE.read()
 
-    if sucess:
-        results=YOLOmodel(frame)
-        count=len(results[0].boxes)
 
+    #if sucess:
+    results=YOLOmodel(frame)
+    count=len(results[0].boxes)
+    
     return count
 
 #Mathod to predict status: crowded or not
@@ -65,10 +79,11 @@ def crowd_status(IMAGE_SOURCE):
         if confidence_score >= 0.9:
             return True
     
-    return True
+    return  False
 
 #Method to redistribute officers if an area is crowded
 def redistribute(): 
+    start_time = time.time()
 
     areas_crowd_count=[] #Takes count of heads detected per image source
     areas_status=[] #Takes crowd classification per image source
@@ -84,22 +99,28 @@ def redistribute():
     for i in areas_crowd_count:
         areas_crowd_count_perc.append(i/sum(areas_crowd_count)) #Calculate crowd percentage for each area
     
-    officers_sum=sum(areas.values()) #Sum of all available officers
+    officers_sum=sum(areas.values()) #100 Sum of all available officers
 
     if (True) in areas_status:
         for index, area in enumerate(areas):
             areas[area]=round(areas_crowd_count_perc[index]*officers_sum)
-        
-        print(areas)
 
-    """
-    Testing purposes only!!!!!!!!!!!!!!!!!!!
+            firestore_current = db.collection("redistribution").document("current")
+            # Set the capital field
+            firestore_current.set(areas)
+        
+        print(areas) # ,areas_status
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Elapsed time: ", elapsed_time) 
     
+    #Testing purposes only!!!!!!!!!!!!!!!!!!!
+    """
     else:
         print("No area is crowded. We have {0} officers, crowd percentages {1}".format(officers_sum, areas_crowd_count_perc))
     """
   
-schedule.every(5).minutes.do(redistribute) 
+schedule.every(0).minutes.do(redistribute) # Minutes could be adjusted as wanted or needed
   
 
 while True: 
